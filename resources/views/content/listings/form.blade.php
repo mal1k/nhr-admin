593d57aa72be1f2a9294a4b284ee4110bf4a45fd
mal1k <00154@ukr.net>
2021-07-14 16:22:49 +0300

    added multiple param

--- resources/views/content/listings/form.blade.php
+++ resources/views/content/listings/form.blade.php
@@ -378,7 +378,7 @@
         </div>
         <div class="col-12 text-danger">
             Gallery (multiple images):<br>
-            <input type="file" name="image_gallery[]">
+            <input type="file" multiple name="image_gallery[]">
             @isset ($listing->image_cover)
             <div class="multi-search-item"><span><img src="{{ asset('/storage/' . $listing->image_cover) }}"></span>
             <input name="image_cover_prev" type="hidden" value="{{ $listing->image_cover }}">
