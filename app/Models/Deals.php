16046a8c75430fb554868cf5fc61dd3de56f48e0
alex <alex@31.134.121>
2021-07-16 18:25:36 +0300

    Fixed table in model

--- app/Models/Deals.php
+++ app/Models/Deals.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Deals extends Model
 {
     use HasFactory;
-    protected $table = 'listings';
+    protected $table = 'deals';
     protected $fillable = [
         'title',
 
@@ -22,6 +22,12 @@ class Deals extends Model
 
         'deal_start_date',
         'deal_end_date',
+
+        'deal_type',
+        'real_price_int',
+        'real_price_cent',
+        'deal_price_int',
+        'deal_price_cent',
 
         'seo_title',
         'seo_page_name',
