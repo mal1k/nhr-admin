895081d7805c348632ece7b939eb0c52a7ad245e
alex <alex@31.134.121>
2021-07-16 14:33:47 +0300

    added fillable fields and casts

--- app/Models/Deals.php
+++ app/Models/Deals.php
@@ -7,9 +7,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Deals extends Model
 {
-    public function index()
-        {
-            $deals = Deals::orderByDesc('id')->paginate(15);
-            return view('content.deals.dashboard', compact('deals'));
-        }
+    use HasFactory;
+    protected $table = 'listings';
+    protected $fillable = [
+        'title',
+
+        'basic_account',
+        'basic_listing',
+        'basic_summary_description',
+        'basic_description',
+        'basic_conditions',
+        'basic_keywords',
+        'basic_mapinfo',
+
+        'deal_start_date',
+        'deal_end_date',
+
+        'seo_title',
+        'seo_page_name',
+        'seo_keywords',
+        'seo_description',
+
+        'image_logo',
+        'image_cover'
+    ];
+    protected $casts = [
+        'basic_keywords' => 'array',
+        'seo_keywords' => 'array'
+    ];
 }
