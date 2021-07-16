a3e1ec60bf8acdbefeba5f549503532e73435c38
alex <alex@31.134.121>
2021-07-16 14:33:03 +0300

    added creating controller

--- app/Http/Controllers/content/dealsController.php
+++ app/Http/Controllers/content/dealsController.php
@@ -4,12 +4,24 @@ namespace App\Http\Controllers\content;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Deals;
 
 class dealsController extends Controller
 {
     public function index()
         {
-            $listings = Listings::orderByDesc('id')->paginate(15);
-            return view('content.listings.dashboard', compact('listings'));
+            $deals = Deals::orderByDesc('id')->paginate(15);
+            return view('content.deals.dashboard', compact('deals'));
         }
+
+    public function create()
+        {
+            $users_query = User::query();
+            $users_query->whereNotNull('business');
+            $users = $users_query->paginate(0);
+            return view('content.deals.form', compact('users'));
+        }
+
+
 }
