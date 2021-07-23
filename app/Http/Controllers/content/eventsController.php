8d402829e094722b9b43d9e8290d0a53ce645bc5
alex <alex@31.134.121>
2021-07-23 13:00:48 +0300

    added store func

--- app/Http/Controllers/content/eventsController.php
+++ app/Http/Controllers/content/eventsController.php
@@ -23,4 +23,40 @@ class eventsController extends Controller
             $users = $users_query->paginate(0);
             return view('content.events.form', compact('users'));
         }
+
+    public function store(Request $request)
+        {
+            $validated = $request->validate([
+                'title' => 'required|max:255',
+            ]);
+
+            // return dd($request->file());
+            $listing = Events::create($request->all()); // create listing
+
+            // upload gallery
+            $images = [];
+            if ($request->hasFile('image_gallery')) {
+                foreach($request->file('image_gallery') as $key => $image){
+                    $images[] = $image->store('uploads/gallery', 'public');
+                }
+                $gallery_path = $images;
+                $listing->update([ 'image_gallery' => $gallery_path ]);
+            }
+
+            if ( isset($request->image_logo) ) {
+                $path = $request->file('image_logo')->store('uploads/logo', 'public'); // upload logo image to server
+                $listing->update([ 'image_logo' => $path ]);
+            }
+
+            return redirect()->route('events.index')->withSuccess('Created listing "' . $request->title . '"');
+        }
+
+    public function edit(Events $event)
+        {
+            $users_query = User::query();
+            $users_query->whereNotNull('business');
+            $users = $users_query->paginate(0);
+
+            return view('content.events.form', compact('event', 'users'));
+        }
 }
