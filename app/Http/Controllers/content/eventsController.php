46ef65e72f50158e03e4a9a69f3b61bddc59f08b
alex <alex@31.134.121>
2021-07-23 14:47:41 +0300

    added image and cover handler

--- app/Http/Controllers/content/eventsController.php
+++ app/Http/Controllers/content/eventsController.php
@@ -43,9 +43,9 @@ class eventsController extends Controller
                 $listing->update([ 'image_gallery' => $gallery_path ]);
             }
 
-            if ( isset($request->image_logo) ) {
-                $path = $request->file('image_logo')->store('uploads/logo', 'public'); // upload logo image to server
-                $listing->update([ 'image_logo' => $path ]);
+            if ( isset($request->image_cover) ) {
+                $path = $request->file('image_cover')->store('uploads/cover', 'public'); // upload cover image to server
+                $listing->update([ 'image_cover' => $path ]);
             }
 
             return redirect()->route('events.index')->withSuccess('Created listing "' . $request->title . '"');
@@ -58,5 +58,51 @@ class eventsController extends Controller
             $users = $users_query->paginate(0);
 
             return view('content.events.form', compact('event', 'users'));
+        }
+
+    public function update(Request $request, Events $event)
+        {
+            $event->update($request->all()); // update event
+
+            if ( isset($request->image_cover) ) { // update cover
+                $path = $request->file('image_cover')->store('uploads/cover', 'public'); // upload cover to server
+                $event->update([ 'image_cover' => $path ]);
+            }
+            if ( empty($request->image_cover_prev) && empty($request->image_cover) ) { // delete cover
+                $event->update([ 'image_cover' => null ]);
+            }
+
+            // upload gallery
+            $images = [];
+            if ($request->hasFile('image_gallery')) {
+                foreach($request->file('image_gallery') as $key => $image){
+                    $images[] = $image->store('uploads/gallery', 'public');
+                }
+                $gallery_path = $images;
+            }
+
+            if ( isset($request->image_gallery_prev) )
+                $gallery_path = array_merge($images, $request->image_gallery_prev);
+
+            if ( isset($gallery_path) )
+                $event->update([ 'image_gallery' => $gallery_path ]);
+            else
+                $event->update([ 'image_gallery' => null ]);
+
+            if ( empty($request->basic_keywords) )
+                $event->update([ 'basic_keywords' => null ]);
+
+            if ( empty($request->seo_keywords) )
+                $event->update([ 'seo_keywords' => null ]);
+
+            return redirect()->route('events.index')->withSuccess('Updated event "' . $request->title . '"');
+        }
+
+    public function destroy(Events $event)
+        {
+            $event->delete();
+
+            return redirect()->route('events.index')->withDanger('Deleted listing "' . $event->title . '"');
+
         }
 }
