f8f60d5485c3906b43816dc4b0d88fa040e3bccc
alex <alex@31.134.121>
2021-07-26 14:16:57 +0300

    added create func

--- app/Http/Controllers/content/bannersController.php
+++ app/Http/Controllers/content/bannersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\content;
 
 use App\Http\Controllers\Controller;
 use App\Models\Banners;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class bannersController extends Controller
@@ -12,5 +13,14 @@ class bannersController extends Controller
         {
             $banners = Banners::orderByDesc('id')->paginate(15);
             return view('content.banners.dashboard', compact('banners'));
+        }
+
+    public function create()
+        {
+            $users_query = User::query();
+            $users_query->whereNotNull('business');
+            $users = $users_query->paginate(0);
+
+            return view('content.banners.form', compact('users'));
         }
 }
