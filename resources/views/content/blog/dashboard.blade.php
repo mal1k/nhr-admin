df06f0d50c25343df85ed65968e5acd8e8c865ea
mal1k <00154@ukr.net>
2021-07-28 02:29:02 +0300

    changed form view

--- resources/views/content/blog/dashboard.blade.php
+++ resources/views/content/blog/dashboard.blade.php
@@ -11,7 +11,6 @@
         <th scope="col">ID</th>
         <th scope="col">Title</th>
         <th scope="col">Status</th>
-        <th scope="col">Categories</th>
         <th scope="col" class="col-2">Actions</th>
         </tr>
     </thead>
@@ -21,7 +20,6 @@
             <th scope="row">{{ $post->id }}</th>
             <td>{{ $post->title }}</td>
             <td>{{ $post->status }}</td>
-            <td>{{ $post->categories }}</td>
             <td>
             <form method="POST" action="{{ route('blog.destroy', $post) }}">
             <a href="{{ route('blog.edit', $post) }}"><button type="button" class="btn-sm btn-secondary">Edit</button></a>
