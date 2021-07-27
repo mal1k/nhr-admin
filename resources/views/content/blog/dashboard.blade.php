8f6078b407ed260183e01af040817ce5b928223d
mal1k <00154@ukr.net>
2021-07-28 01:31:07 +0300

    created dashboard

---
+++ resources/views/content/blog/dashboard.blade.php
@@ -0,0 +1,39 @@
+@extends ('layout')
+
+@section('title', 'Blog')
+
+@section('content')
+<a href="{{ route('blog.create') }}"><button type="button" class="btn btn-primary mb-3">{{ __('Create post') }}</button></a>
+
+    <table class="table table-sm">
+    <thead>
+        <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Title</th>
+        <th scope="col">Status</th>
+        <th scope="col">Categories</th>
+        <th scope="col" class="col-2">Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+    @foreach($blog as $post)
+        <tr>
+            <th scope="row">{{ $post->id }}</th>
+            <td>{{ $post->title }}</td>
+            <td>{{ $post->status }}</td>
+            <td>{{ $post->categories }}</td>
+            <td>
+            <form method="POST" action="{{ route('blog.destroy', $blog) }}">
+            <a href="{{ route('blog.edit', $blog) }}"><button type="button" class="btn-sm btn-secondary">Edit</button></a>
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn-sm btn-danger">Delete</button>
+            </form>
+            </td>
+        </tr>
+    @endforeach
+    </tbody>
+    </table>
+
+    {{ $blog->links() }}
+@endsection
