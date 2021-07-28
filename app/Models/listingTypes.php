a9afc098d3ac5e53604a06a848b22d2c923281db
mal1k <00154@ukr.net>
2021-07-28 15:36:52 +0300

    added model

---
+++ app/Models/listingTypes.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class listingTypes extends Model
+{
+    use HasFactory;
+    protected $table = 'listing_types';
+    protected $fillable = [
+    'title',
+    'categories',
+    'additional_price',
+    'common_fields_listing_title_label',
+    'common_fields_listing_title_tooltip',
+    'common_fields_address_line_label',
+    'common_fields_address_line_tooltip',
+    'common_fields_address_line2_label',
+    'common_fields_address_line2_tooltip',
+    'extra_checkbox_fields_label',
+    'extra_checkbox_fields_tooltip',
+    'extra_dropdown_fields_label',
+    'extra_dropdown_fields_dropdown_items',
+    'extra_dropdown_fields_tooltip',
+    'extra_dropdown_fields_checkbox',
+    'extra_text_fields_label',
+    'extra_text_fields_tooltip',
+    'extra_text_fields_checkbox',
+    'extra_short_description_fields_label',
+    'extra_short_description_fields_tooltip',
+    'extra_short_description_fields_checkbox',
+    'extra_long_description_fields_label',
+    'extra_long_description_fields_tooltip',
+    'extra_long_description_fields_checkbox'
+    ];
+}
