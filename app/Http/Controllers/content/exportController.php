41b35c310739a441d6e90e13ff770a618bf9b8c4
mal1k <00154@ukr.net>
2021-07-29 20:53:07 +0300

    added cloud export

--- app/Http/Controllers/content/exportController.php
+++ app/Http/Controllers/content/exportController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers\content;
 
 use App\Exports\bannersExport;
-use App\Http\Controllers\Controller;
 use App\Exports\faqExport;
+
+use App\Http\Controllers\Controller;
 use App\Models\exportContent;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
@@ -18,15 +19,24 @@ class exportController extends Controller
 
     public function cloudexport(Request $request) {
         $name = $request->category;
-        $name = $name.'_'.date('d-m-Y_H-i').'_' .time();
-        // return dd($request->all());
+        $filename = $name . '_' . date('d-m-Y_H-i-s').'.csv';
+        $classname = "App\Exports\\" . $request->category;
+
+        exportContent::create([
+            'filename' => $filename,
+            'filesize' => 123,
+        ]);
+
+        Excel::store(new $classname, 'exportContent/'.$filename, 'local');
+
         return redirect()->route('export.index')->withSuccess('Created export file "' . $request->category . '" on server');
     }
 
     public function localexport(Request $request) {
         $name = $request->category;
-        $filename = $name . '_' . date('d-m-Y_H-i');
+        $filename = $name . '_' . date('d-m-Y_H-i-s');
         $classname = "App\Exports\\" . $request->category;
         return Excel::download(new $classname, $filename.'.csv');
     }
+
 }
