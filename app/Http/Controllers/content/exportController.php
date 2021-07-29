a67999be571fbe7818e6ae3a6ace8270ef6caa2d
mal1k <00154@ukr.net>
2021-07-29 18:31:18 +0300

    added local export func

--- app/Http/Controllers/content/exportController.php
+++ app/Http/Controllers/content/exportController.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Controllers\content;
 
+use App\Exports\bannersExport;
 use App\Http\Controllers\Controller;
+use App\Exports\faqExport;
+use App\Models\exportContent;
 use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
 
 class exportController extends Controller
 {
     public function index() {
-        return view('content.export.dashboard');
+        $exportList = exportContent::orderByDesc('id')->paginate(15);
+        return view('content.export.dashboard', compact('exportList'));
+    }
+
+    public function cloudexport(Request $request) {
+        $name = $request->category;
+        $name = $name.'_'.date('d-m-Y_H-i').'_' .time();
+        // return dd($request->all());
+        return redirect()->route('export.index')->withSuccess('Created export file "' . $request->category . '" on server');
+    }
+
+    public function localexport(Request $request) {
+        $name = $request->category;
+        $filename = $name . '_' . date('d-m-Y_H-i');
+        $classname = "App\Exports\\" . $request->category;
+        return Excel::download(new $classname, $filename.'.csv');
     }
 }
