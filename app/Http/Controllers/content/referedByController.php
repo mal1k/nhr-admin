07639161154fa84ff57b833dcf85b2c84eb9a7f5
mal1k <00154@ukr.net>
2021-07-31 17:47:00 +0300

    added index func for refered by

---
+++ app/Http/Controllers/content/referedByController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\content;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class referedByController extends Controller
+{
+    public function index()
+        {
+            $users = User::orderByDesc('id')->whereNotNull('refered_by')->paginate(15);
+            return view('content.referedBy.dashboard', compact('users'));
+        }
+}
