38a4e9b52ad7e0b5fd6e2d1abea4f3df12509160
mal1k <00154@ukr.net>
2021-08-03 13:59:34 +0300

    created migration for event categories

---
+++ database/migrations/2021_08_03_104010_create_event_categories_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event_categories', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->json('categories')->nullable();
+
+            $table->string('features_checkbox')->nullable();
+            $table->string('disable_checkbox')->nullable();
+
+            $table->string('main_category')->nullable();
+
+            $table->text('content')->nullable();
+
+            $table->string('seo_page_title')->nullable();
+            $table->string('seo_friendly_title')->nullable();
+            $table->json('seo_keywords')->nullable();
+            $table->string('seo_description')->nullable();
+
+            $table->string('image_logo')->nullable();
+            $table->string('image_icon')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event_categories');
+    }
+}
