1c17c3a41407739a334267556217d69bbb8f4968
mal1k <00154@ukr.net>
2021-08-03 13:58:28 +0300

    fixed new item

--- resources/views/content/events/categories/form.blade.php
+++ resources/views/content/events/categories/form.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <div class="my-3">
-  <a class="text-decoration-none text-dark" href="{{ route('listing-categories.index') }}">
+  <a class="text-decoration-none text-dark" href="{{ route('event-categories.index') }}">
     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="margin-top: -3px;margin-right: -3px;"><line x1="20" y1="12" x2="4" y2="12"></line><polyline points="10 18 4 12 10 6"></polyline></svg>
     Back
   </a>
@@ -12,9 +12,9 @@
 
 <form method="POST" enctype="multipart/form-data"
     @if ( isset($category) )
-        action="{{ route('listing-categories.update', $category) }}"
+        action="{{ route('event-categories.update', $category) }}"
     @else
-        action="{{ route('listing-categories.store') }}"
+        action="{{ route('event-categories.store') }}"
     @endif
     class="row row-cols-lg-auto g-3 align-items-center">
     @csrf
@@ -167,7 +167,7 @@
 </form>
 
 @isset ($category)
-  <form class="col" method="POST" action="{{ route('listing-categories.destroy', $category) }}">
+  <form class="col" method="POST" action="{{ route('event-categories.destroy', $category) }}">
       @csrf
       @method('DELETE')
       <button type="submit" class="btn btn-danger mb-3">Delete</button>
