aa32fde585502ac75435f55aecd666ab13e5e4b8
mal1k <00154@ukr.net>
2021-08-05 18:28:26 +0300

    updated template

--- resources/views/content/referedBy/dashboard.blade.php
+++ resources/views/content/referedBy/dashboard.blade.php
@@ -1,35 +1,73 @@
-@extends ('layout')
+@extends ('new-layout')
 
 @section('title', 'Refered By')
 
 @section('content')
-    <table class="table table-sm">
-    <thead>
-        <tr>
-        <th scope="col">ID</th>
-        <th scope="col">Referring User</th>
-        <th scope="col">Referred User</th>
-        <!-- <th scope="col" class="col-2">Actions</th> -->
-        </tr>
-    </thead>
-    <tbody>
-    @foreach($users as $info)
-        <tr>
-            <th scope="row">{{ $info->id }}</th>
-            <td>{{ $info->email }}</td>
-            <td>{{ $info->refered_by }}</td>
-            <!-- <td>
-            <form method="POST" action="{{ route('faq.destroy', $info) }}">
-            <a href="{{ route('faq.edit', $info) }}"><button type="button" class="btn-sm btn-secondary">Edit</button></a>
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn-sm btn-danger">Delete</button>
-            </form>
-            </td> -->
-        </tr>
-    @endforeach
-    </tbody>
-    </table>
+
+<div class="card mb-5 mb-xl-8">
+    <!--begin::Header-->
+    <div class="card-header border-0 pt-5">
+        <h3 class="card-title align-items-start flex-column">
+            <span class="card-label fw-bolder fs-3 mb-1">Refered By</span>
+        </h3>
+        <div class="card-toolbar">
+            <a href="{{ route('refered_by.create') }}" class="btn btn-sm btn-light-primary">
+                <!--begin::Svg Icon | path: icons/duotone/Communication/Add-user.svg-->
+                <span class="svg-icon svg-icon-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                        <path d="M18,8 L16,8 C15.4477153,8 15,7.55228475 15,7 C15,6.44771525 15.4477153,6 16,6 L18,6 L18,4 C18,3.44771525 18.4477153,3 19,3 C19.5522847,3 20,3.44771525 20,4 L20,6 L22,6 C22.5522847,6 23,6.44771525 23,7 C23,7.55228475 22.5522847,8 22,8 L20,8 L20,10 C20,10.5522847 19.5522847,11 19,11 C18.4477153,11 18,10.5522847 18,10 L18,8 Z M9,11 C6.790861,11 5,9.209139 5,7 C5,4.790861 6.790861,3 9,3 C11.209139,3 13,4.790861 13,7 C13,9.209139 11.209139,11 9,11 Z" fill="#000000" fill-rule="nonzero" opacity="0.3" />
+                        <path d="M0.00065168429,20.1992055 C0.388258525,15.4265159 4.26191235,13 8.98334134,13 C13.7712164,13 17.7048837,15.2931929 17.9979143,20.2 C18.0095879,20.3954741 17.9979143,21 17.2466999,21 C13.541124,21 8.03472472,21 0.727502227,21 C0.476712155,21 -0.0204617505,20.45918 0.00065168429,20.1992055 Z" fill="#000000" fill-rule="nonzero" />
+                    </svg>
+                </span>
+                <!--end::Svg Icon-->Create a referral user</a>
+        </div>
+
+    </div>
+    <!--end::Header-->
+    <!--begin::Body-->
+    <div class="card-body py-3">
+        <!--begin::Table container-->
+        <div class="table-responsive">
+            <!--begin::Table-->
+            <table class="table align-middle gs-0 gy-4">
+                <!--begin::Table head-->
+                <thead>
+                    <tr class="fw-bolder text-muted bg-light">
+                        <th class="ps-4 min-w-25px rounded-start">ID</th>
+                        <th class="min-w-225px">Referring User</th>
+                        <th class="min-w-225px">Referred User</th>
+                        <!-- <th class="min-w-200px text-end rounded-end"></th> -->
+                    </tr>
+                </thead>
+                <!--end::Table head-->
+                <!--begin::Table body-->
+                <tbody>
+                    @foreach($users as $info)
+                    <tr>
+                        <td>
+                            <div class="d-flex align-items-center">
+                                <div class="d-flex justify-content-start flex-column">
+                                    <a href="#" class="text-dark fw-bolder mb-1 fs-6">{{ $info->id }}</a>
+                                </div>
+                            </div>
+                        </td>
+                        <td>
+                            <a href="#" class="text-dark fw-bolder d-block mb-1 fs-6">{{ $info->email }}</a>
+                        </td>
+                        <td>
+                            <a href="#" class="text-dark fw-bolder d-block mb-1 fs-6">{{ $info->refered_by }}</a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+                <!--end::Table body-->
+            </table>
+            <!--end::Table-->
+        </div>
+        <!--end::Table container-->
+    </div>
+    <!--begin::Body-->
+</div>
 
     {{ $users->links() }}
 @endsection
