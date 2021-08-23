69f656ab5753fa4a5ff97d1f4dc4d2306ad7f57e
mal1k <00154@ukr.net>
2021-08-23 20:43:33 +0300

    added public sortable

--- app/Models/Faq.php
+++ app/Models/Faq.php
@@ -4,15 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Kyslik\ColumnSortable\Sortable;
 
 class Faq extends Model
 {
     use HasFactory;
+    use Sortable;
     protected $table = 'faq';
     protected $fillable = [
     'question',
     'answer',
     'front',
     'sponsors'
+    ];
+    public $sortable = [
+        'id',
+        'question',
+        'answer',
     ];
 }
