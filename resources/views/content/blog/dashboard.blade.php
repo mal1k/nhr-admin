51e0951024dbbe1371ee24fe9a1233cd0edaef0f
mal1k <00154@ukr.net>
2021-08-23 20:19:36 +0300

    added search input

--- resources/views/content/blog/dashboard.blade.php
+++ resources/views/content/blog/dashboard.blade.php
@@ -45,10 +45,17 @@
                 <!--begin::Table head-->
                 <thead>
                     <tr class="fw-bolder text-muted bg-light">
-                        <th class="ps-4 min-w-25px rounded-start">ID</th>
-                        <th class="min-w-225px">Title</th>
-                        <th class="min-w-200px">Status</th>
-                        <th class="min-w-200px text-end rounded-end"></th>
+                        <th class="ps-4 min-w-25px rounded-start">@sortablelink('id', 'ID')</th>
+                        <th class="min-w-225px">@sortablelink('title', 'Title')</th>
+                        <th class="min-w-200px">@sortablelink('status', 'Status')</th>
+                        <th class="min-w-200px text-end rounded-end">
+                            <form action="">
+                                <div class="row m-0">
+                                    <input type="text" class="col form-control" placeholder="Search" name="s" value="@isset($search) {{ $search }} @endisset">
+                                    <a href="{{ route('blog.index') }}" class="col-2 btn btn-icon btn-bg-light btn-active-color-primary"><i class="fas fa-sync-alt"></i></a>
+                                </div>
+                            </form>
+                        </th>
                     </tr>
                 </thead>
                 <!--end::Table head-->
