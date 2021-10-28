8867716a8f8fef6a2b8b463598bb189f77ad6acb
mal1k <00154@ukr.net>
2021-10-28 18:47:43 +0300

    added pagebuilder to menu

--- resources/views/new-layout.blade.php
+++ resources/views/new-layout.blade.php
@@ -287,6 +287,25 @@
                                     <span class="menu-title">Refered by</span>
                                 </a>
                             </div>
+
+                            <div class="menu-item">
+                                <a id="pagebuilderLogin" class="menu-link @if(app()->view->getSections()['title'] == 'Page Builder') active @endif" href="/pagebuilder">
+                                    <span class="menu-icon">
+                                        <!--begin::Svg Icon | path: icons/duotone/Design/Difference.svg-->
+                                        <span class="svg-icon svg-icon-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                                    <rect x="0" y="0" width="24" height="24" />
+                                                    <rect fill="#000000" x="4" y="4" width="7" height="7" rx="1.5" />
+                                                    <path d="M5.5,13 L9.5,13 C10.3284271,13 11,13.6715729 11,14.5 L11,18.5 C11,19.3284271 10.3284271,20 9.5,20 L5.5,20 C4.67157288,20 4,19.3284271 4,18.5 L4,14.5 C4,13.6715729 4.67157288,13 5.5,13 Z M14.5,4 L18.5,4 C19.3284271,4 20,4.67157288 20,5.5 L20,9.5 C20,10.3284271 19.3284271,11 18.5,11 L14.5,11 C13.6715729,11 13,10.3284271 13,9.5 L13,5.5 C13,4.67157288 13.6715729,4 14.5,4 Z M14.5,13 L18.5,13 C19.3284271,13 20,13.6715729 20,14.5 L20,18.5 C20,19.3284271 19.3284271,20 18.5,20 L14.5,20 C13.6715729,20 13,19.3284271 13,18.5 L13,14.5 C13,13.6715729 13.6715729,13 14.5,13 Z" fill="#000000" opacity="0.3" />
+                                                </g>
+                                            </svg>
+                                        </span>
+                                        <!--end::Svg Icon-->
+                                    </span>
+                                    <span class="menu-title">Page Builder</span>
+                                </a>
+                            </div>
                         </div>
                         <!--end::Menu-->
                     </div>
@@ -6211,6 +6230,13 @@
             )
         </script>
     @endif
+
+    <script>
+        $('#pagebuilderLogin').click(function() {
+            $.post("/pagebuilder/auth?action=login", { username: "admin", password: "^L86jLe8b!" })
+                .done(function(data) {});
+        })
+    </script>
     <!--end::Javascript-->
 </body>
 </html>
